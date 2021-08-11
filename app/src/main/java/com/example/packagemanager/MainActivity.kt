8821a7843c551ceecb33c988c0e282da429c2691
kj <79355765+kj27july@users.noreply.github.com>
2021-08-11 20:29:00 +0530

    unistall added

--- app/src/main/java/com/example/packagemanager/MainActivity.kt
+++ app/src/main/java/com/example/packagemanager/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.packagemanager
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -11,46 +13,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val youtubeInstalled: Boolean = appInstalled("com.google.android.youtube")
-        if (youtubeInstalled) {
-            Log.d("akshay-y", "youtube Installed");
-        } else
-            Log.d("akshay-y", "youtube not Installed");
+     val intent:Intent  = Intent(Intent.ACTION_DELETE)
+        intent.setData(Uri.parse("package:com.example.packagemanager"))
+        startActivity(intent)
+
+//        val youtubeInstalled: Boolean = appInstalled("com.google.android.youtube")
+//        if (youtubeInstalled) {
+//            Log.d("akshay-y", "youtube Installed");
+//        } else
+//            Log.d("akshay-y", "youtube not Installed");
 
 //all package name
-        val packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-        for (packageInfo in packages) {
-            Log.d("akshay", "Package name:" + packageInfo.packageName)
-        }
+//        val packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+//        for (packageInfo in packages) {
+//            Log.d("akshay", "Package name:" + packageInfo.packageName)
+//        }
     }
 
-        val packages2 = packageManager.getInstallSourceInfo("com.google.android.youtube")
-
-        val chkInstall= isPackageInstalled("com.google.android.youtube",packageManager)
+//        val packages2 = packageManager.getInstallSourceInfo("com.google.android.youtube")
+//        val chkInstall= isPackageInstalled("com.google.android.youtube",packageManager)
 
 
 
 //if package install
-    private fun appInstalled(uri: String): Boolean {
-        val pm = packageManager
-        var app_installed = false
-        app_installed = try {
-            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
-        return app_installed
-    }
+//    private fun appInstalled(uri: String): Boolean {
+//        val pm = packageManager
+//        var app_installed = false
+//        app_installed = try {
+//            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
+//            true
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            false
+//        }
+//        return app_installed
+//    }
 
-    private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
-        return try {
-            packageManager.getPackageInfo(packageName, 0)
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
-    }
+//    private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
+//        return try {
+//            packageManager.getPackageInfo(packageName, 0)
+//            true
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            false
+//        }
+//    }
 
 
 }
